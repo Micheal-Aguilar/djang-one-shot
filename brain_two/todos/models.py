f60fb3821bd347fd2791c35bdff8321cae0e60fa
Micheal <mikey4426@gmail.com>
2023-08-29 10:39:22 -0700

    Feature 3 Complete

--- brain_two/todos/models.py
+++ brain_two/todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class ToDoList(models.Model):
+    name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
